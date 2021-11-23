c1297b66a4c6f070759442ec834b12f4c6f600a6
anshu vishwakarma <anshuvishwakarma923@gmail.com>
2021-11-23 12:47:04 +0530

    Updates

---
+++ precache-manifest.924087b355419ccc91a1e89ea086edc1.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d324352112a904301a326621f9cb287",
+    "url": "/portfolioresume/index.html"
+  },
+  {
+    "revision": "7f2df4b02df4ef06471f",
+    "url": "/portfolioresume/static/css/main.28026575.chunk.css"
+  },
+  {
+    "revision": "9f94cbbea05b189c0632",
+    "url": "/portfolioresume/static/js/2.8ce567ba.chunk.js"
+  },
+  {
+    "revision": "e0120c6fcbfd33b267f706d8402aed5a",
+    "url": "/portfolioresume/static/js/2.8ce567ba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7f2df4b02df4ef06471f",
+    "url": "/portfolioresume/static/js/main.34fc2735.chunk.js"
+  },
+  {
+    "revision": "1abf50895009f5bf26ce",
+    "url": "/portfolioresume/static/js/runtime-main.fd546d76.js"
+  },
+  {
+    "revision": "041ae30b966d4e61220c52d856e9fda8",
+    "url": "/portfolioresume/static/media/centralia-coin.041ae30b.jpg"
+  },
+  {
+    "revision": "392a8ca62d7d8abd63e628e1b78d391e",
+    "url": "/portfolioresume/static/media/projectpic.392a8ca6.jpg"
+  },
+  {
+    "revision": "ac623cc2f8044c89386cde2bf2f4e82d",
+    "url": "/portfolioresume/static/media/reactsaastemplate.ac623cc2.jpg"
+  }
+]);
